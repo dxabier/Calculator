77e02180097dddcbaed3fc2d1a4dcfd384e23df9
dxabier <denispicn@gmail.com>
2023-11-13 22:34:22 -0600

    Antes de prevenir mas de dos puntos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 function App() {
   let [input, setInput] = useState([]);
   let [numero, setNumero] = useState(0);
+  let [signo, setSigno] = useState(0);
+  let [punto, setPunto] = useState(0);
 
 
   const manejo = (e) => {
@@ -16,24 +18,32 @@ function App() {
     let nuevo = [...input];
     
     if (algo === '1' || algo === '2' || algo === '3' || algo === '4' || algo === '5' || algo === '6' || algo === '7' || algo === '8' || algo === '9') { setNumero(++numero)};
+    if (algo == '+' || algo == '-' || algo == '*' || algo == "/") {setSigno(++signo); setPunto(0)}
+    if (algo == '.') {setPunto(++punto);}
 
-    if (nuevo[0] == '0' && nuevo.indexOf('.') < 0 && numero == 0) {
+    if (nuevo[0] == '0' && nuevo.indexOf('.') < 0 && numero == 0 && signo == 0 && punto == 0) {
       console.log('para cero inical')
+
     } else if ((nuevo[nuevo.length-1] == '+' && algo == '+') || (nuevo[nuevo.length-1] == '-' && algo == '-') || (nuevo[nuevo.length-1] == '*' && algo == '*') || (nuevo[nuevo.length-1] == '/' && algo == '/')) {
       console.log('dos igualres')
+
     } else if (nuevo[nuevo.length-1] == '+' &&  (algo == '-' || algo == '*' || algo == '/' ) || nuevo[nuevo.length-1] == '-' &&  (algo == '+' || algo == '*' || algo == '/' ) || nuevo[nuevo.length-1] == '*' &&  (algo == '-' || algo == '+' || algo == '/' ) || nuevo[nuevo.length-1] == '/' &&  (algo == '-' || algo == '*' || algo == '+' )) {
       console.log('cambio signo')      
       nuevo[nuevo.length-1] = algo
       setInput(nuevo)
+
+    } else if ((nuevo[nuevo.length-1] == '.' && algo == '.') ) {      
+      console.log('doble punto')
+
+    } else if (punto >= 0 && signo == 0) {
+      console.log('mas de un punto en el mismo')
     }
      else {
-      if (nuevo[0] == '0') {nuevo[0] = ''}
+      // if (nuevo[0] == '0') {nuevo[0] = ''}
       nuevo.push(algo)
       setInput(nuevo)
     }
-    
-    
-    
+        
   }
   
 
@@ -55,7 +65,7 @@ function App() {
         <div id="seven" className='col pad' onClick={manejo}>7</div>
         <div id="eight" className='col pad'onClick={manejo}>8</div>
         <div id="nine" className='col pad' onClick={manejo}>9</div>
-        <div id="clear" className='col pad ac' onClick={() => {setInput(''); setNumero(0)}}>AC</div>        
+        <div id="clear" className='col pad ac' onClick={() => {setInput(''); setNumero(0); setSigno(0);setPunto(0)}}>AC</div>        
       </div>
       <div className='row'>
         <div id="four" className='col pad' onClick={manejo}>4</div>
